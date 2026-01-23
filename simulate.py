d7567de9e0735f0b003f0e579aa3f53da9039a86
calli <cmlevitt@uvm.edu>
2026-01-23 15:44:59 -0500

    generate and simulate updates files- part C finished

--- simulate.py
+++ simulate.py
@@ -1,6 +1,9 @@
 import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+#physicsClient = p.connect(p.GUI)
+p.loadSDF("box.sdf")
 for i in range(1000):
     time.sleep(1/60)
     p.stepSimulation()
