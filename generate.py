d4ee27396b5eab12f47d8a04fad09813b980322f
calli <cmlevitt@uvm.edu>
2026-01-31 15:38:05 -0500

    step 45: joints

--- generate.py
+++ generate.py
@@ -13,6 +13,13 @@ def Create_World():
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,1,1.5] , size=[length, width, height])
+    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,0.5,1.5])
+    pyrosim.Send_Cube(name="BackLeg", pos=[0,0,0.5] , size=[length, width, height])
+    pyrosim.End()
+
+
+'''
     pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5] , size=[length, width, height])
     pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
     pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5] , size=[length, width, height])
@@ -27,6 +34,6 @@ def Create_Robot():
     pyrosim.Send_Joint(name = "Link5_Link6" , parent= "Link5" , child = "Link6" , type = "revolute", position = [0,0,-1.0])
     pyrosim.Send_Cube(name="Link6", pos=[0,0,-1.0] , size=[length, width, height])
     pyrosim.End()
-
+'''
 Create_World()
 Create_Robot()
