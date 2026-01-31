f367842e5bef200953e39ff2373b825f85b6ed9e
calli <cmlevitt@uvm.edu>
2026-01-31 14:59:50 -0500

    step 34 joints

--- generate.py
+++ generate.py
@@ -4,21 +4,18 @@ pyrosim.Start_SDF("world.sdf")
 length = 1
 width = 1
 height = 1
-x= 0
-y=0
-z=0.5
 
 #one link 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[-5,3,z] , size=[length, width, height])
+    pyrosim.Send_Cube(name="Box", pos=[-5,3,0.5] , size=[length, width, height])
     pyrosim.End()
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length, width, height])
-    pyrosim.Send_Joint(name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [.5,0,1])
-    pyrosim.Send_Cube(name="Leg", pos=[1.0,y,1.5] , size=[length, width, height])
+    pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5] , size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5] , size=[length, width, height])
     pyrosim.End()
 
 Create_World()
