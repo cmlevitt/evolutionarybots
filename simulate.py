d42baf770820c6d46d2500d7a35a3cecae221edd
calli <cmlevitt@uvm.edu>
2026-02-06 12:12:27 -0500

    motors started

--- simulate.py
+++ simulate.py
@@ -23,6 +23,12 @@ for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robotId,
+    jointName = "BackLegJoint",
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = 0.0,
+    maxForce = 500)
     #print(i)
     
 #print(backLegSensorValues)
