58d79af4b012d5b02e7e8125909ffa01c14fd247
calli <cmlevitt@uvm.edu>
2026-02-16 11:12:47 -0500

    motors doneeee

--- generate.py
+++ generate.py
@@ -8,7 +8,7 @@ height = 1
 #one link 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[-5,3,0.5] , size=[length, width, height])
+    #pyrosim.Send_Cube(name="Box", pos=[-5,3,0.5] , size=[length, width, height])
     pyrosim.End()
 
 def Create_Robot():
--- simulate.py
+++ simulate.py
@@ -6,9 +6,13 @@ import time
 import pyrosim.pyrosim as pyrosim
 import numpy as np
 
-amplitude = pi/4
-frequency = 10
-phaseOffset = 0
+amplitude_bk = pi/4
+frequency_bk = 10
+phaseOffset_bk = 0
+
+amplitude_fr = pi/4
+frequency_fr = 10
+phaseOffset_fr = -0.5
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -29,9 +33,13 @@ frontLegSensorValues = np.zeros(1000)
 targetAngles = np.sin(values)
 targetAngles = targetAngles * (pi/4) 
 '''
-targetAngles = np.zeros(1000)
+targetAngles_bk = np.zeros(1000)
 for i in range (1000):
-    targetAngles[i] = amplitude * np.sin(frequency * 2*pi*i/1000 + phaseOffset)
+    targetAngles_bk[i] = amplitude_bk * np.sin(frequency_bk * 2*pi*i/1000 + phaseOffset_bk)
+
+targetAngles_fr = np.zeros(1000)
+for i in range (1000):
+    targetAngles_fr[i] = amplitude_fr * np.sin(frequency_fr * 2*pi*i/1000 + phaseOffset_fr)
 '''
 np.save("data/SinusoidallyVaryingVals.npy", targetAngles)
 exit() '''
@@ -46,20 +54,20 @@ for i in range(1000):
         bodyIndex = robotId,
         jointName = b'Torso_BackLeg',
         controlMode = p.POSITION_CONTROL,
-        targetPosition = targetAngles[i], 
+        targetPosition = targetAngles_bk[i], 
         maxForce = 100)
 
     pyrosim.Set_Motor_For_Joint(
         bodyIndex = robotId,
         jointName = b'Torso_FrontLeg',
         controlMode = p.POSITION_CONTROL,
-        targetPosition = targetAngles[i],
+        targetPosition = targetAngles_fr[i],
         maxForce = 100)
         #print(i)
 
 #print(backLegSensorValues)
 #np.save("data/backLegSensorValues.npy", backLegSensorValues)
 #np.save("data/frontLegSensorValues.npy", frontLegSensorValues)
-np.save("data/SinusoidallyVaryingValsFront.npy", targetAngles)
-np.save("data/SinusoidallyVaryingValsBack.npy", targetAngles)
+np.save("data/SinusoidallyVaryingValsFront.npy", targetAngles_fr)
+np.save("data/SinusoidallyVaryingValsBack.npy", targetAngles_bk)
 p.disconnect()
