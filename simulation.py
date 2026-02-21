325161d4b1f70e3dc1e1280f0eb8f61c1ad68d4e
calli <cmlevitt@uvm.edu>
2026-02-21 12:59:52 -0500

    step 40

--- simulation.py
+++ simulation.py
@@ -1,9 +1,47 @@
 from world import WORLD
 from robot import ROBOT
+import pybullet as p
+import pybullet_data
+import time
+import pyrosim.pyrosim as pyrosim
 
 class SIMULATION:
 
     def __init__(self):
 
+        physicsClient = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+        p.setGravity(0,0,-9.8,physicsClient)
+
         self.world = WORLD()
         self.robot = ROBOT()
+        pyrosim.Prepare_To_Simulate(self.robot.robotId)
+        
+
+    def Run(self):
+        for i in range(1000):
+            time.sleep(1/60)
+            p.stepSimulation()
+            '''
+            backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+            frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+            pyrosim.Set_Motor_For_Joint(
+                bodyIndex = self.robot.robotId,
+                jointName = b'Torso_BackLeg',
+                controlMode = p.POSITION_CONTROL,
+                targetPosition = targetAngles_bk[i], 
+                maxForce = 100)
+
+            pyrosim.Set_Motor_For_Joint(
+                bodyIndex = self.robot.robotId,
+                jointName = b'Torso_FrontLeg',
+                controlMode = p.POSITION_CONTROL,
+                targetPosition = targetAngles_fr[i],
+                maxForce = 100)
+                '''
+            print(i)
+    
+            
+                ##step 27
