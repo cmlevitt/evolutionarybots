d35ba887dbb0fd8579f4f778697d51e903dff83c
calli <cmlevitt@uvm.edu>
2026-04-11 10:12:20 -0400

    bot starts on platform

--- solution.py
+++ solution.py
@@ -71,13 +71,62 @@ class SOLUTION:
             time.sleep(0.01)
 
     def Generate_Body(self):
-
         length = 1
         width = 1
         height = 1
+
+        x_off = c.ar_length/2 + (c.platform_thickness - c.side_wall_thickness) / 2
+        z_off = 0.1 + c.ar_height + 1
+
+        # Deltas from original torso position [0, 0, 1]
+        dx = x_off - 0
+        dz = z_off - 1
+
         pyrosim.Start_URDF("body.urdf")
 
-        pyrosim.Send_Cube(name="Torso", pos=[0,0,1] , size=[length, width, height])
+        pyrosim.Send_Cube(name="Torso", pos=[x_off, 0, z_off], size=[length, width, height])
+
+        # Joint positions shifted by dx/dz, cube pos values unchanged
+        pyrosim.Send_Joint(name="Torso_BackLeg",  parent="Torso", child="BackLeg",  type="revolute", position=[dx+0,  -0.5, dz+1.0], jointAxis="1 0 0")
+        pyrosim.Send_Cube(name="BackLeg",  pos=[0,-0.5,0], size=[0.2,1,0.2])
+
+        pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[dx+0,   0.5, dz+1.0], jointAxis="1 0 0")
+        pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,0], size=[0.2,1,0.2])
+
+        pyrosim.Send_Joint(name="Torso_LeftLeg",  parent="Torso", child="LeftLeg",  type="revolute", position=[dx-0.5,  0,  dz+1.0], jointAxis="0 1 0")
+        pyrosim.Send_Cube(name="LeftLeg",  pos=[-0.5,0,0], size=[1,0.2,0.2])
+
+        pyrosim.Send_Joint(name="Torso_RightLeg", parent="Torso", child="RightLeg", type="revolute", position=[dx+0.5,  0,  dz+1.0], jointAxis="0 1 0")
+        pyrosim.Send_Cube(name="RightLeg", pos=[0.5,0,0], size=[1,0.2,0.2])
+
+        pyrosim.Send_Joint(name="FrontLeg_FrontLowerLeg", parent="FrontLeg", child="FrontLowerLeg", type="revolute", position=[0, 1, 0], jointAxis="1 0 0")
+        pyrosim.Send_Cube(name="FrontLowerLeg", pos=[0,0,-0.5], size=[0.2,0.2,1])
+
+        pyrosim.Send_Joint(name="BackLeg_BackLowerLeg",   parent="BackLeg",  child="BackLowerLeg",  type="revolute", position=[0, -1, 0], jointAxis="1 0 0")
+        pyrosim.Send_Cube(name="BackLowerLeg",  pos=[0,0,-0.5], size=[0.2,0.2,1])
+
+        pyrosim.Send_Joint(name="LeftLeg_LeftLowerLeg",   parent="LeftLeg",  child="LeftLowerLeg",  type="revolute", position=[-1, 0, 0], jointAxis="0 1 0")
+        pyrosim.Send_Cube(name="LeftLowerLeg",  pos=[0,0,-0.5], size=[0.2,0.2,1.0])
+
+        pyrosim.Send_Joint(name="RightLeg_RightLowerLeg", parent="RightLeg", child="RightLowerLeg", type="revolute", position=[1, 0, 0], jointAxis="0 1 0")
+        pyrosim.Send_Cube(name="RightLowerLeg", pos=[0,0,-0.5], size=[0.2,0.2,1.0])
+
+        pyrosim.End()
+        '''
+        length = 1
+        width = 1
+        height = 1
+
+        # Spawn offset to place robot on right platform
+        x_off = c.ar_length/2 + (c.platform_thickness - c.side_wall_thickness) / 2
+        z_off = 0.1 + c.ar_height + 1  # platform top + leg height
+        pyrosim.Start_URDF("body.urdf")
+
+        platform_x = c.ar_length/2 + (c.platform_thickness - c.side_wall_thickness) / 2
+        platform_top_z = 0.1 + c.ar_height  # floor base + wall/platform height
+
+        pyrosim.Send_Cube(name="Torso", pos=[x_off, 0, z_off + 1], size=[length, width, height])
+        #pyrosim.Send_Cube(name="Torso", pos=[0,0,1] , size=[length, width, height])
         
         pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,-0.5,1.0], jointAxis = "1 0 0")
         pyrosim.Send_Cube(name="BackLeg", pos=[0, -0.5, 0] , size=[0.2,1,0.2],)
@@ -106,7 +155,7 @@ class SOLUTION:
         pyrosim.End()
         while not os.path.exists("body.urdf"):
             time.sleep(0.01)
-
+'''
     def Generate_Arena(self, length=c.ar_length, width=c.ar_width, height=c.ar_height,
                    side_wall_thickness=c.side_wall_thickness, platform_thickness=c.platform_thickness,
                    floor_thickness=c.floor_thickness):
